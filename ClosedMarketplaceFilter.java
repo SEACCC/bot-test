c228e58eb55b98d0d677a972201ab4ff04643ca0
revad-ueda <revad_ueda@yahoo.co.jp>
2017-12-21 12:18:05 +0900

    Update ClosedMarketplaceFilter.java

--- ClosedMarketplaceFilter.java
+++ ClosedMarketplaceFilter.java
@@ -80,6 +80,11 @@ public class ClosedMarketplaceFilter extends BaseBesFilter implements Filter {
             VOUserDetails voUserDetails = (VOUserDetails) httpRequest
                     .getSession().getAttribute(Constants.SESS_ATTR_USER);
 
+            if (mId.equals("") || configBean == null) {
+                 chain.doFilter(request, response);
+                return;
+            }
+        
             MarketplaceConfiguration config = configBean.getConfiguration(mId,
                     httpRequest);
 
