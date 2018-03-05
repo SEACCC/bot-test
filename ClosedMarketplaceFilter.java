c20fbaf5545f558ccdf469105640ecddae4de1b7
revad-ueda <revad_ueda@yahoo.co.jp>
2018-03-05 15:51:13 +0900

    Update ClosedMarketplaceFilter.java

--- ClosedMarketplaceFilter.java
+++ ClosedMarketplaceFilter.java
@@ -12,11 +12,6 @@ import static org.oscm.ui.beans.BaseBean.ERROR_PAGE;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
